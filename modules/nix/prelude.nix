50b36902ca35b0b337b0665b4b8d41caec935142
ttak0422 <ttak0422@gmail.com>
2021-11-22 01:43:39 +0900

    binary caches

--- modules/nix/prelude.nix
+++ modules/nix/prelude.nix
@@ -10,5 +10,8 @@
       automatic = true;
       options = "--delete-older-than 10d";
     };
+    binaryCaches = [ "https://ttak0422.cachix.org" ];
+    binaryCachePublicKeys =
+      [ "ttak0422.cachix.org-1:nv62UZqX8/xe5jSyyTu4Z7tSpX1TiCYaYv89Xe7CFaM=" ];
   };
 }

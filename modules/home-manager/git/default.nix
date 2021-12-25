35dae1811874d96c4ec2ff9f9cba46785989d7be
ttak0422 <ttak0422@gmail.com>
2021-12-26 00:00:17 +0900

    remove emoji

--- modules/home-manager/git/default.nix
+++ modules/home-manager/git/default.nix
@@ -29,21 +29,21 @@ let
 
 
     # ==== Emojis ====
-    # 🎨 :art: when improving the format/structure of the code
-    # 🐎 :racehorse: when improving performance
-    # 🚱 :non-potable_water: when plugging memory leaks
-    # 📝 :memo: when writing docs
-    # 🐧 :penguin: when fixing something on Linux
-    # 🍎 :apple: when fixing something on macOS
-    # 🏁 :checkered_flag: when fixing something on Windows
-    # 🐛 :bug: when fixing a bug
-    # 🔥 :fire: when removing code or files
-    # 💚 :green_heart: when fixing the CI build
-    # ✅ :white_check_mark: when adding tests
-    # 🔒 :lock: when dealing with security
-    # ⬆️ :arrow_up: when upgrading dependencies
-    # ⬇️ :arrow_down: when downgrading dependencies
-    # 👕 :shirt: when removing linter warnings
+    # :art: when improving the format/structure of the code
+    # :racehorse: when improving performance
+    # :non-potable_water: when plugging memory leaks
+    # :memo: when writing docs
+    # :penguin: when fixing something on Linux
+    # :apple: when fixing something on macOS
+    # :checkered_flag: when fixing something on Windows
+    # :bug: when fixing a bug
+    # :fire: when removing code or files
+    # :green_heart: when fixing the CI build
+    # :white_check_mark: when adding tests
+    # :lock: when dealing with security
+    # :arrow_up: when upgrading dependencies
+    # :arrow_down: when downgrading dependencies
+    # :shirt: when removing linter warnings
   '';
   extraConfig = {
     inherit core color;

26e284368b05058e3d94c5905b032053a9f04008
ttak0422 <ttak0422@gmail.com>
2022-01-01 04:29:24 +0900

    :+1: echo doc

--- modules/home-manager/vim/neovim.nix
+++ modules/home-manager/vim/neovim.nix
@@ -235,6 +235,14 @@ let
     # wip...
     # https://github.com/chentau/marks.nvim
     # https://github.com/numToStr/FTerm.nvim
+    {
+      plugin = echodoc-vim;
+      config = ''
+        let g:echodoc#enable_at_startup = 1
+        let g:echodoc#type = 'floating'
+        highlight link EchoDocFloat Pmenu
+      '';
+    }
   ]) ++ [
     external.fzy-lua-native
   ]

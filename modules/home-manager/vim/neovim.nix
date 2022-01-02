cbc6cbc77b87ba0a00547b755e5691cb3f2e27ed
ttak0422 <ttak0422@gmail.com>
2022-01-02 23:00:48 +0900

    gitdiff

--- modules/home-manager/vim/neovim.nix
+++ modules/home-manager/vim/neovim.nix
@@ -545,7 +545,16 @@ let
     -- vim.cmd [[autocmd CursorHold,CursorHoldI * lua require'nvim-lightbulb'.update_lightbulb()]]
 
     -- gitsigns
-    require('gitsigns').setup()
+    require('gitsigns').setup {
+      status_formatter = function(status)
+        local added, changed, removed = status.added, status.changed, status.removed
+        local status_txt = {}
+        if added   and added   > 0 then table.insert(status_txt, ' '..added  ) end
+        if changed and changed > 0 then table.insert(status_txt, ' '..changed) end
+        if removed and removed > 0 then table.insert(status_txt, ' '..removed) end
+        return table.concat(status_txt, ' ')
+      end
+    }
 
     -- stabilize
     require('stabilize').setup()

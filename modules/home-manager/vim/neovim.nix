b5e63136eb36229b88d45cc44219b81daef3b221
ttak0422 <ttak0422@gmail.com>
2022-01-19 01:16:00 +0900

    :construction: zen-mode優先

--- modules/home-manager/vim/neovim.nix
+++ modules/home-manager/vim/neovim.nix
@@ -95,10 +95,6 @@ let
         let g:oceanic_next_terminal_bold = 1
         let g:oceanic_next_terminal_italic = 1
         colorscheme OceanicNext
-        hi Normal guibg=NONE ctermbg=NONE
-        hi LineNr guibg=NONE ctermbg=NONE
-        hi SignColumn guibg=NONE ctermbg=NONE
-        hi EndOfBuffer guibg=NONE ctermbg=NONE
       '';
     }
 

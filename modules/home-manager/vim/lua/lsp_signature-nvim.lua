d4873c02708b2f7c52331b98ac60483eb613e86a
ttak0422 <ttak0422@gmail.com>
2022-01-25 00:53:11 +0900

    :fire: unused files

--- modules/home-manager/vim/lua/lsp_signature-nvim.lua
+++
@@ -1,6 +0,0 @@
-require "lsp_signature".setup({
-  bind = true,
-  handler_opts = {
-    border = "rounded"
-  }
-})
--- modules/home-manager/vim/lua/lspsaga-nvim.lua
+++
@@ -1,5 +0,0 @@
--- WIP
-local saga = require 'lspsaga'
-
-saga.init_lsp_saga {
-}

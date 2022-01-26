38dc28c76f96724e29307ca39c64a21ee320d45f
ttak0422 <ttak0422@gmail.com>
2022-01-26 22:06:11 +0900

    :+1: indent-blankline

--- modules/home-manager/vim/lua/packer/cfg/indent-blanklin-nvim.lua
+++ modules/home-manager/vim/lua/packer/cfg/indent-blanklin-nvim.lua
@@ -1,5 +1,10 @@
 
 require('indent_blankline').setup {
-    show_end_of_line = true,
-    space_char_blankline = ' ',
+  after = 'nvim-treesitter',
+  show_end_of_line = true,
+  space_char_blankline = ' ',
+  use_treesitter = true,
+  show_current_context = true,
+  filetype_exclude = { 'alpha' , 'lspinfo', 'packer', 'checkhealth', 'help', ''},
 }
+

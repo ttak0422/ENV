0fa11bd74aa01aca2715ddb892f3a62f01f7f6c3
ttak0422 <ttak0422@gmail.com>
2022-02-09 01:21:42 +0900

    lsp diagnostics icon

--- modules/home-manager/vim/lua/packer/cfg/nvim-lsp-installer.lua
+++ modules/home-manager/vim/lua/packer/cfg/nvim-lsp-installer.lua
@@ -57,3 +57,14 @@ end)
 vim.diagnostic.config {
   severity_sort = true
 }
+
+local signs = {
+	{ name = 'DiagnosticSignError', text = '' },
+	{ name = 'DiagnosticSignWarn', text = '' },
+	{ name = 'DiagnosticSignHint', text = '' },
+	{ name = 'DiagnosticSignInfo', text = '' },
+}
+
+for _, sign in ipairs(signs) do
+	vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = '' })
+end

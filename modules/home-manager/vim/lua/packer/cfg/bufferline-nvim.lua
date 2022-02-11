cd4fd98b5ad31d5996ed813a791822244aa2c868
ttak0422 <ttak0422@gmail.com>
2022-02-11 15:05:54 +0900

    :art: change align

--- modules/home-manager/vim/lua/packer/cfg/bufferline-nvim.lua
+++ modules/home-manager/vim/lua/packer/cfg/bufferline-nvim.lua
@@ -6,7 +6,7 @@ require'bufferline'.setup{
         filetype = 'NvimTree',
         text = 'File Explorer',
         highlight = 'Directory',
-        text_align = 'left'
+        text_align = 'center'
       }
     },
   },

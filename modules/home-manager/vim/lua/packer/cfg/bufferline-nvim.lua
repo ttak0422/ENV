e152bd39fb0dc8c635532adc89f7620194f46233
ttak0422 <ttak0422@gmail.com>
2022-02-11 16:35:44 +0900

    :art: simplify bufferline

--- modules/home-manager/vim/lua/packer/cfg/bufferline-nvim.lua
+++ modules/home-manager/vim/lua/packer/cfg/bufferline-nvim.lua
@@ -1,6 +1,9 @@
 require'bufferline'.setup{
   options = {
+    show_buffer_icons = false,
+    show_buffer_close_icons = false,
     show_close_icon = false,
+    separator_style = 'slant',
     offsets = {
       {
         filetype = 'NvimTree',

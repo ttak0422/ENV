50d697d614a3b00115dd93f128490af24b5a99a5
ttak0422 <ttak0422@gmail.com>
2022-02-27 11:50:03 +0900

    change: borderless

--- modules/home-manager/vim/lua/packer/cfg/nvim-lsp-installer.lua
+++ modules/home-manager/vim/lua/packer/cfg/nvim-lsp-installer.lua
@@ -1,20 +1,20 @@
-local border = {
-  {'╭', 'FloatBorder'},
-  {'─', 'FloatBorder'},
-  {'╮', 'FloatBorder'},
-  {'│', 'FloatBorder'},
-  {'╯', 'FloatBorder'},
-  {'─', 'FloatBorder'},
-  {'╰', 'FloatBorder'},
-  {'│', 'FloatBorder'},
-}
-
-local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
-function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
-  opts = opts or {}
-  opts.border = opts.border or border
-  return orig_util_open_floating_preview(contents, syntax, opts, ...)
-end
+-- local border = {
+--   {'╭', 'FloatBorder'},
+--   {'─', 'FloatBorder'},
+--   {'╮', 'FloatBorder'},
+--   {'│', 'FloatBorder'},
+--   {'╯', 'FloatBorder'},
+--   {'─', 'FloatBorder'},
+--   {'╰', 'FloatBorder'},
+--   {'│', 'FloatBorder'},
+-- }
+--
+-- local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+-- function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+--   opts = opts or {}
+--   opts.border = opts.border or border
+--   return orig_util_open_floating_preview(contents, syntax, opts, ...)
+-- end
 
 local on_attach = function(client, bufnr)
   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end

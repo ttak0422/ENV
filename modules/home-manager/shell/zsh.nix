de817cdff722cc62145927badd72591397f0ce57
ttak0422 <ttak0422@gmail.com>
2022-03-05 16:20:58 +0900

    fmt

--- modules/home-manager/shell/zsh.nix
+++ modules/home-manager/shell/zsh.nix
@@ -57,25 +57,25 @@ in {
       ${functionConfig}
       ${keybindConfig}
 
-            # https://qiita.com/ssh0/items/a9956a74bff8254a606a
-            if [[ ! -n $TMUX ]]; then
-              # get the IDs
-              ID="`tmux list-sessions`"
-              if [[ -z "$ID" ]]; then
-                tmux new-session
-              fi
-              create_new_session="Create New Session"
-              ID="$ID\n''${create_new_session}:"
-              ID="`echo $ID | fzf --no-sort --prompt="Session > " | cut -d: -f1`"
-              if [[ "$ID" = "''${create_new_session}" ]]; then
-                tmux new-session
-              elif [[ -n "$ID" ]]; then
-                tmux attach-session -t "$ID"
-              else
-                :  # Start terminal normally
-              fi
-            fi
-            '';
+      # https://qiita.com/ssh0/items/a9956a74bff8254a606a
+      if [[ ! -n $TMUX ]]; then
+        # get the IDs
+        ID="`tmux list-sessions`"
+        if [[ -z "$ID" ]]; then
+          tmux new-session
+        fi
+        create_new_session="Create New Session"
+        ID="$ID\n''${create_new_session}:"
+        ID="`echo $ID | fzf --no-sort --prompt="Session > " | cut -d: -f1`"
+        if [[ "$ID" = "''${create_new_session}" ]]; then
+          tmux new-session
+        elif [[ -n "$ID" ]]; then
+          tmux attach-session -t "$ID"
+        else
+          :  # Start terminal normally
+        fi
+      fi
+    '';
     sessionVariables = { EDITOR = "vim"; };
     plugins = [
       {

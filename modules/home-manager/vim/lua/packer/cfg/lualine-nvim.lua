72730779adb8c29d029cb833500ba094910b7445
ttak0422 <ttak0422@gmail.com>
2022-04-07 23:18:19 +0900

    fix lualine

--- modules/home-manager/vim/lua/packer/cfg/lualine-nvim.lua
+++ modules/home-manager/vim/lua/packer/cfg/lualine-nvim.lua
@@ -159,14 +159,8 @@ ins_right({
 })
 
 ins_right({
-  function () return '' end,
-  padding = { left = 0, right = 0 },
-  color = { fg = colors.subBg },
-})
-ins_right({
   'location',
   padding = { left = 1, right = 1 },
-  color = { fg = colors.subFg, bg = colors.subBg },
 })
 ins_right({
   'o:encoding',

61371337a2f01bb012f3bff5c6e6b9b2beb1933f
ttak0422 <ttak0422@gmail.com>
2022-04-19 20:12:29 +0900

    fixup

--- modules/homebrew/work.nix
+++ modules/homebrew/work.nix
@@ -1,24 +1,27 @@
 { config, pkgs, lib, ... }: {
   imports = [ ./prelude.nix ];
-  taps = [
-    "homebrew/cask"
-    "homebrew/cask-drivers"
-    "homebrew/cask-fonts"
-    "homebrew/core"
-    "koekeishiya/formulae"
-  ];
-  casks = [
-    "alacritty"
-    "cyberduck"
-    "displaylink"
-    "firefox"
-    "font-fira-code"
-    "font-hack-nerd-font"
-    "iterm2"
-    "jetbrains-toolbox"
-    "karabiner-elements"
-    "keycastr"
-    "tableplus"
-    "visual-studio-code"
-  ];
+  homebrew = {
+    brews = [ "lunchy" ];
+    taps = [
+      "homebrew/cask"
+      "homebrew/cask-drivers"
+      "homebrew/cask-fonts"
+      "homebrew/core"
+      "koekeishiya/formulae"
+    ];
+    casks = [
+      "alacritty"
+      "cyberduck"
+      "displaylink"
+      "firefox"
+      "font-fira-code"
+      "font-hack-nerd-font"
+      "iterm2"
+      "jetbrains-toolbox"
+      "karabiner-elements"
+      "keycastr"
+      "tableplus"
+      "visual-studio-code"
+    ];
+  };
 }

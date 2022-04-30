2eab1cffde36eba58314abacc9a7fe31c5d55062
ttak0422 <ttak0422@gmail.com>
2022-05-01 02:37:05 +0900

    change tsconfig

--- modules/home-manager/vim/lua/packer/cfg/nvim-treesitter.lua
+++ modules/home-manager/vim/lua/packer/cfg/nvim-treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = 'maintained',
+  ensure_installed = { 'bash', 'c', 'c_sharp', 'clojure', 'cmake', 'commonlisp', 'cpp', 'css', 'd', 'dart', 'dockerfile', 'elixir', 'elm', 'erlang', 'fennel', 'fish', 'go', 'gomod', 'gowork', 'graphql', 'haskell', 'html', 'java', 'javascript', 'jsdoc', 'json', 'jsonc', 'json5', 'julia', 'kotlin', 'lua', 'make', 'markdown', 'nix', 'ocaml', 'ocaml', 'ocaml_interface', 'python', 'r', 'ruby', 'rust', 'scala', 'scss', 'swift', 'toml', 'tsx', 'typescript', 'vim', 'vue', 'yaml', },
   sync_install = false,
   ignore_install = {},
   highlight = {

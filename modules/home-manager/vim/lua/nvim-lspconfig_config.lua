94d63f487d88cf4c5db655d7f46c0a3e4ead4fa4
ttak0422 <ttak0422@gmail.com>
2022-06-26 18:08:54 +0900

    modify lsp-config

--- modules/home-manager/vim/lua/nvim-lspconfig_config.lua
+++ modules/home-manager/vim/lua/nvim-lspconfig_config.lua
@@ -35,12 +35,16 @@ vim.diagnostic.config {
 }
 
 local signs = {
-	{ name = 'DiagnosticSignError', text = '' },
-	{ name = 'DiagnosticSignWarn', text = '' },
-	{ name = 'DiagnosticSignHint', text = '' },
-	{ name = 'DiagnosticSignInfo', text = '' },
+	{ name = 'DiagnosticSignError', text = '' },
+	{ name = 'DiagnosticSignWarn', text = '' },
+	{ name = 'DiagnosticSignHint', text = '' },
+	{ name = 'DiagnosticSignInfo', text = '' },
 }
 
 for _, sign in ipairs(signs) do
 	vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = '' })
 end
+
+require('lspconfig').jdtls.setup {
+  use_lombok_agent = true
+}

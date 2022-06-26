12e784a460063a43919ab7e3cc6654a1887c3edf
ttak0422 <ttak0422@gmail.com>
2022-06-26 18:09:49 +0900

    modify flare

--- modules/home-manager/vim/lua/flare_config.lua
+++ modules/home-manager/vim/lua/flare_config.lua
@@ -1,16 +1,1 @@
-require'flare'.setup {
-  enabled = true,
-  hl_group = "IncSearch",
-  x_threshold = 10,
-  y_threshold = 5,
-  expanse = 10,
-  file_ignore = {
-    "NvimTree",
-    "TelescopePrompt",
-    "TelescopeResult",
-    "Trouble",
-  },
-  fade = true,
-  underline = false,
-  timeout = 150,
-}
+require'flare'.setup {}

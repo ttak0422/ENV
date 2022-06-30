ec7ad41fb533d65508de26b1a1fa72e431c04772
ttak0422 <ttak0422@gmail.com>
2022-07-01 00:11:10 +0900

    update: jvm

--- modules/home-manager/dev/jvm.nix
+++ modules/home-manager/dev/jvm.nix
@@ -1,7 +1,7 @@
 { config, pkgs, lib, ... }: {
   home = {
-    packages = with pkgs; [ jdk11 scala gradle maven sbt google-java-format ];
-    file = { ".jdk/openjdk".source = pkgs.jdk11; };
+    packages = with pkgs; [ jdk scala gradle maven sbt google-java-format ];
+    file = { ".jdk/openjdk".source = pkgs.jdk; };
     sessionVariables = { JAVA_HOME = "$HOME/.jdk/openjdk"; };
   };
 }

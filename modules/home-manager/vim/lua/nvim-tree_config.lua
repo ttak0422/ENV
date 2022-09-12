d57cb67fab0e77040d1b30014594f0f3996307ce
ttak0422 <ttak0422@gmail.com>
2022-09-13 00:22:02 +0900

    Update nvim-tree bindings

--- modules/home-manager/vim/lua/nvim-tree_config.lua
+++ modules/home-manager/vim/lua/nvim-tree_config.lua
@@ -1,9 +1,22 @@
-require'nvim-tree'.setup {
+require 'nvim-tree'.setup {
   hijack_cursor = true,
   view = {
     width = 35,
     side = 'left',
     hide_root_folder = true,
+    adaptive_size = true,
+    mappings = {
+      custom_only = true,
+      list = {
+        { key = { '<CR>', 'o' }, action = 'edit' },
+        { key = 'O', action = 'system_open' },
+        { key = 'a', action = 'create' },
+        { key = 'd', action = 'remove' },
+        { key = 'r', action = 'rename' },
+        { key = 'R', action = 'refresh' },
+        { key = '?', action = 'toggle_help' },
+      },
+    },
   },
   update_focused_file = {
     enable = true,

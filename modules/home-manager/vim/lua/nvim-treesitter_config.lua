cc0ca37eca77da71a2f960c770824557df71c6c6
ttak0422 <ttak0422@gmail.com>
2022-09-27 01:50:12 +0900

    Fix: append

--- modules/home-manager/vim/lua/nvim-treesitter_config.lua
+++ modules/home-manager/vim/lua/nvim-treesitter_config.lua
@@ -1,8 +1,14 @@
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = { 'bash', 'c', 'c_sharp', 'clojure', 'cmake', 'commonlisp', 'cpp', 'css', 'd', 'dart', 'dockerfile', 'elixir', 'elm', 'erlang', 'fennel', 'fish', 'go', 'gomod', 'gowork', 'graphql', 'haskell', 'html', 'java', 'javascript', 'jsdoc', 'json', 'jsonc', 'json5', 'julia', 'kotlin', 'lua', 'make', 'markdown', 'nix', 'ocaml', 'ocaml', 'ocaml_interface', 'python', 'r', 'ruby', 'rust', 'scala', 'scss', 'toml', 'tsx', 'typescript', 'vim', 'vue', 'yaml', 'org' },
+local parser_install_dir = vim.fn.stdpath('data') .. '/treesitter_parser'
+vim.opt.runtimepath:append(parser_install_dir)
+require 'nvim-treesitter.configs'.setup {
+  ensure_installed = { 'bash', 'c', 'c_sharp', 'clojure', 'cmake', 'commonlisp', 'cpp', 'css', 'd', 'dart', 'dockerfile',
+    'elixir', 'elm', 'erlang', 'fennel', 'fish', 'go', 'gomod', 'gowork', 'graphql', 'haskell', 'html', 'java',
+    'javascript', 'jsdoc', 'json', 'jsonc', 'json5', 'julia', 'kotlin', 'lua', 'make', 'markdown', 'nix', 'ocaml',
+    'ocaml', 'ocaml_interface', 'python', 'r', 'ruby', 'rust', 'scala', 'scss', 'toml', 'tsx', 'typescript', 'vim', 'vue',
+    'yaml', 'org' },
   sync_install = false,
   ignore_install = {},
-  parser_install_dir = vim.fn.stdpath('data') .. '/treesitter_parser/',
+  parser_install_dir = parser_install_dir,
   highlight = {
     enable = true,
     ignore_install = { 'c' },

4f9dc6233a3d925ceb75b706b55a4559df022ea3
ttak0422 <ttak0422@gmail.com>
2022-10-04 01:07:39 +0900

    Update: roam cfg

--- modules/home-manager/emacs/emacs.d/init.el
+++ modules/home-manager/emacs/emacs.d/init.el
@@ -126,7 +126,20 @@
   :ensure t
   :hook (org-mode-hook . (lambda () (org-bullets-mode 1))))
 (leaf org-roam
-  :ensure t)
+  :ensure t
+  :custom
+  ((org-roam-directory . "~/org-roam"))
+  :bind
+  (("C-c n l" . org-roam-buffer-toggle)
+   ("C-c n f" . org-roam-node-find)
+   ("C-c n g" . org-roam-graph)
+   ("C-c n i" . org-roam-node-insert)
+   ("C-c n c" . org-roam-capture)
+   ("C-c n j" . org-roam-dailies-capture-today))
+  :config
+  (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+  (org-roam-db-autosync-mode)
+  )
 (leaf org-roam-ui
   :ensure t
   :after org-roam

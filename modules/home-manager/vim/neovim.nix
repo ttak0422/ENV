cc402a14f690a2cee869f2e20aa526e3f35ea8f6
ttak0422 <ttak0422@gmail.com>
2022-10-09 20:20:17 +0900

    Refactor: bufopts

--- modules/home-manager/vim/neovim.nix
+++ modules/home-manager/vim/neovim.nix
@@ -306,12 +306,12 @@ let
           local bufopts = { noremap = true, silent = true, buffer = bufnr }
           vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
           vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-          vim.keymap.set('n', 'gh', '<cmd>Lspsaga lsp_finder<CR>', { silent = true })
+          vim.keymap.set('n', 'gh', '<cmd>Lspsaga lsp_finder<CR>', bufopts)
 
           vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
           vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
           -- vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-          vim.keymap.set('n', 'K', '<cmd>Lspsaga hover_doc<CR>', { silent = true })
+          vim.keymap.set('n', 'K', '<cmd>Lspsaga hover_doc<CR>', bufopts)
           vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
 
           vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
@@ -320,10 +320,10 @@ let
 
           vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
           -- vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-          vim.keymap.set('n', 'rn', '<cmd>Lspsaga rename<CR>', { silent = true })
+          vim.keymap.set('n', 'rn', '<cmd>Lspsaga rename<CR>', bufopts)
 
           -- vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-          vim.keymap.set('n', '<leader>ca', '<cmd>Lspsaga code_action<CR>', { silent = true })
+          vim.keymap.set('n', '<leader>ca', '<cmd>Lspsaga code_action<CR>', bufopts)
           vim.keymap.set('n', '<space>f', vim.lsp.buf.format, bufopts)
 
         end
@@ -935,4 +935,3 @@ in {
     withPython3 = true;
   };
 }
-

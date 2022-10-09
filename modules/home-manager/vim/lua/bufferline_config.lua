e8026068582ae3eed3bf1db9e6412da3616b527c
ttak0422 <ttak0422@gmail.com>
2022-10-10 01:15:19 +0900

    Update: apply highlight

--- modules/home-manager/vim/lua/bufferline_config.lua
+++ modules/home-manager/vim/lua/bufferline_config.lua
@@ -1,16 +1,17 @@
-require'bufferline'.setup{
+require("bufferline").setup({
   options = {
     show_buffer_icons = false,
     show_buffer_close_icons = false,
     show_close_icon = false,
-    separator_style = 'thick',
+    separator_style = "thick",
     offsets = {
       {
-        filetype = 'NvimTree',
-        text = 'File Explorer',
-        highlight = 'Directory',
-        text_align = 'center'
-      }
+        filetype = "NvimTree",
+        text = "File Explorer",
+        highlight = "Directory",
+        text_align = "center",
+      },
     },
+    highlights = require("catppuccin.groups.integrations.bufferline").get(),
   },
-}
+})

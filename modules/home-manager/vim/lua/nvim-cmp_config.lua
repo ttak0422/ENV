9f3b52d6ac651ce253897a13509b5419634bceaa
ttak0422 <ttak0422@gmail.com>
2022-10-10 15:06:31 +0900

    Update: nvim-cmp keymap

--- modules/home-manager/vim/lua/nvim-cmp_config.lua
+++ modules/home-manager/vim/lua/nvim-cmp_config.lua
@@ -45,6 +45,13 @@ cmp.setup({
 				fallback()
 			end
 		end, { "i", "s" }),
+		["<C-l>"] = cmp.mapping(function(fallback)
+			if luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
 	}),
 	sources = cmp.config.sources({
 		{ name = "nvim_lsp", priority = 10 },

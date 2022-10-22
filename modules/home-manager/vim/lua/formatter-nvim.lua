7c0ed6961c8418b1c48adc2475eeec9f81402ffe
ttak0422 <ttak0422@gmail.com>
2022-10-22 14:13:19 +0900

    Change: stylua config

--- modules/home-manager/vim/lua/formatter-nvim.lua
+++ modules/home-manager/vim/lua/formatter-nvim.lua
@@ -1,30 +1,30 @@
 local util = require("formatter.util")
 
 require("formatter").setup({
-	logging = true,
-	log_level = vim.log.levels.WARN,
-	filetype = {
-		lua = {
-			require("formatter.filetypes.lua").stylua,
-		},
-		java = {
-			function()
-				return {
-					exe = "google-java-format",
-					args = {
-						"-",
-					},
-					stdin = true,
-				}
-			end,
-		},
-		nix = {
-			function()
-				return {
-					exe = "nixfmt",
-					stdin = true,
-				}
-			end,
-		},
-	},
+  logging = true,
+  log_level = vim.log.levels.WARN,
+  filetype = {
+    lua = {
+      require("formatter.filetypes.lua").stylua,
+    },
+    java = {
+      function()
+        return {
+          exe = "google-java-format",
+          args = {
+            "-",
+          },
+          stdin = true,
+        }
+      end,
+    },
+    nix = {
+      function()
+        return {
+          exe = "nixfmt",
+          stdin = true,
+        }
+      end,
+    },
+  },
 })

59a5c7e2209f4f6314b3a89f2db95979b6a23aad
ttak0422 <ttak0422@gmail.com>
2022-10-22 14:15:10 +0900

    Update: telescope ui-select

--- modules/home-manager/vim/lua/telescope-nvim_config.lua
+++ modules/home-manager/vim/lua/telescope-nvim_config.lua
@@ -2,35 +2,35 @@ local telescope = require("telescope")
 local lga_actions = require("telescope-live-grep-args.actions")
 
 telescope.setup({
-	defaults = require("telescope.themes").get_ivy({
-		path_display = { "truncate" },
-		prompt_prefix = " ",
-		selection_caret = " ",
-	}),
-	extensions = {
-		file_browser = {
-			theme = "ivy",
-			mappings = {
-				["i"] = {},
-				["n"] = {},
-			},
-		},
-		live_grep_args = {
-			auto_quoting = true,
-			mappings = { -- extend mappings
-				i = {
-					["<C-k>"] = lga_actions.quote_prompt(),
-				},
-			},
-		},
-		command_palette = {
-			{ "Vim", { "check health", ":checkhealth" } },
-		},
-	},
+  defaults = require("telescope.themes").get_ivy({
+    path_display = { "truncate" },
+    prompt_prefix = " ",
+    selection_caret = " ",
+  }),
+  extensions = {
+    file_browser = {
+      theme = "ivy",
+      mappings = {
+        ["i"] = {},
+        ["n"] = {},
+      },
+    },
+    live_grep_args = {
+      auto_quoting = true,
+      mappings = { -- extend mappings
+        i = {
+          ["<C-k>"] = lga_actions.quote_prompt(),
+        },
+      },
+    },
+    ["ui-select"] = {
+      require("telescope.themes").get_ivy({}),
+    },
+  },
 })
 
 telescope.load_extension("live_grep_args")
 telescope.load_extension("projects")
 telescope.load_extension("file_browser")
-telescope.load_extension("command_palette")
 telescope.load_extension("sonictemplate")
+telescope.load_extension("ui-select")

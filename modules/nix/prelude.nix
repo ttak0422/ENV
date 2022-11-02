765534c98efa9a6d9974ac83416da19c95ceb3ca
ttak0422 <ttak0422@gmail.com>
2022-11-02 22:56:17 +0900

    Fix: nix config

--- modules/nix/prelude.nix
+++ modules/nix/prelude.nix
@@ -14,11 +14,13 @@
       substituters = [
         "https://ttak0422.cachix.org"
         "https://ttak0422-vim-plugins-overlay.cachix.org"
+        "https://nix-community.cachix.org"
         "https://cachix.org/api/v1/cache/emacs"
       ];
       trusted-public-keys = [
         "ttak0422.cachix.org-1:nv62UZqX8/xe5jSyyTu4Z7tSpX1TiCYaYv89Xe7CFaM="
         "ttak0422-vim-plugins-overlay.cachix.org-1:YqM0x7zLhQtcskLyiCGp810p/C7WjQ94jp9CJ7UY46E="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "emacs.cachix.org-1:b1SMJNLY/mZF6GxQE+eDBeps7WnkT0Po55TAyzwOxTY="
       ];
     };

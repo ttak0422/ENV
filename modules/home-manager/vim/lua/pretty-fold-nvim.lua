b4c8fcc7724661c7c2ba161dd45929734ac73b73
ttak0422 <ttak0422@gmail.com>
2022-11-02 22:57:58 +0900

    Remove: preview

--- modules/home-manager/vim/lua/pretty-fold-nvim.lua
+++ modules/home-manager/vim/lua/pretty-fold-nvim.lua
@@ -1,18 +1,20 @@
-require('pretty-fold').setup {
+require("pretty-fold").setup({
   -- keep_indentation = false,
   --keep_indentation = true,
-  fill_char = '•',
+  fill_char = "•",
   sections = {
     left = {
-      function() return string.rep(' ', vim.o.shiftwidth * vim.v.foldlevel) end,
-      ' ',
-      'number_of_folded_lines',
-      ': ',
-      'content',
-      ' ',
+      function()
+        return string.rep(" ", vim.o.shiftwidth * vim.v.foldlevel)
+      end,
+      " ",
+      "number_of_folded_lines",
+      ": ",
+      "content",
+      " ",
     },
   },
-}
-require('fold-preview').setup {
-  default_keybindings = false;
-}
+})
+-- require('fold-preview').setup {
+--   default_keybindings = false;
+-- }

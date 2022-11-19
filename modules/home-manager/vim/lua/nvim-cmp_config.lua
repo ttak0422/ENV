20bf2d2e253f19bf93fee78e5790e1a470b269b7
ttak0422 <ttak0422@gmail.com>
2022-11-19 22:58:00 +0900

    Remove: unused

--- modules/home-manager/vim/lua/nvim-cmp_config.lua
+++ modules/home-manager/vim/lua/nvim-cmp_config.lua
@@ -27,8 +27,6 @@ cmp.setup({
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif vim.fn["vsnip#available"](1) == 1 then
-        feedkey("<Plug>(vsnip-expand-or-jump)", "")
       else
         fallback()
       end
@@ -36,8 +34,6 @@ cmp.setup({
     ["<S-Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-        feedkey("<Plug>(vsnip-jump-prev)", "")
       else
         fallback()
       end

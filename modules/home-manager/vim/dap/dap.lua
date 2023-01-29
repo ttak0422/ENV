e4c622012acb63616205451395d0e6e24cde765f
ttak0422 <ttak0422@gmail.com>
2023-01-29 13:28:16 +0900

    Update: dap

--- modules/home-manager/vim/dap/dap.lua
+++ modules/home-manager/vim/dap/dap.lua
@@ -17,10 +17,10 @@ dapui.setup({
 })
 
 dap.listeners.before.event_terminated["dapui_config"] = function()
-  dapui.close()
+  -- dapui.close()
 end
 dap.listeners.before.event_exited["dapui_config"] = function()
-  dapui.close()
+  -- dapui.close()
 end
 
 require("nvim-dap-virtual-text").setup({

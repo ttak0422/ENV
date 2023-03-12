41c4feb52b1c660fbd3fda51228a1f080e13d70d
ttak0422 <ttak0422@gmail.com>
2023-03-12 19:34:10 +0900

    modify: rust lsp

--- modules/home-manager/vim/lsp/rust-tools.lua
+++ modules/home-manager/vim/lsp/rust-tools.lua
@@ -4,6 +4,13 @@ return function(opt)
     server = {
       on_attach = opt.on_attach,
       capabilities = opt.capabilities,
+      settings = {
+        ["rust-analyzer"] = {
+          files = {
+            excludeDirs = { ".direnv" },
+          },
+        },
+      },
     },
   })
   rt.inlay_hints.enable()

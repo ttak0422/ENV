2299df4c4c16e0179153bc4ec79e664abf3f968c
ttak0422 <ttak0422@gmail.com>
2023-03-14 22:06:47 +0900

    Update: cargo config

--- modules/home-manager/dev/rust.nix
+++ modules/home-manager/dev/rust.nix
@@ -1,1 +1,20 @@
-{ config, pkgs, lib, ... }: { home.packages = with pkgs; [ rustup ]; }
+{ config, pkgs, lib, ... }: {
+  home = {
+    packages = with pkgs; [ rustup ];
+    file = {
+      ".cargo/config".text = ''
+        [target.x86_64-apple-darwin]
+        rustflags = [
+          "-C", "link-arg=-undefined",
+          "-C", "link-arg=dynamic_lookup",
+        ]
+
+        [target.aarch64-apple-darwin]
+        rustflags = [
+          "-C", "link-arg=-undefined",
+          "-C", "link-arg=dynamic_lookup",
+        ]
+      '';
+    };
+  };
+}

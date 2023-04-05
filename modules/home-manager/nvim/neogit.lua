444e80e73f8f9293022d4171501c945274fcdcfd
ttak0422 <ttak0422@gmail.com>
2023-04-06 01:43:41 +0900

    modify

--- modules/home-manager/nvim/neogit.lua
+++ modules/home-manager/nvim/neogit.lua
@@ -1,6 +1,11 @@
 require("neogit").setup({
+  kind = "split",
   disable_commit_confirmation = true,
   integrations = {
     diffview = true,
+  },
+  signs = {
+    section = { "", "" },
+    item = { "", "" },
   },
 })

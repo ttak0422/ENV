721d0470abe9c6a91c7a9f85df7aeb505a4b53a8
ttak0422 <ttak0422@gmail.com>
2023-05-04 16:47:12 +0900

    modify: defaults

--- modules/darwin/system/keyboard.nix
+++ modules/darwin/system/keyboard.nix
@@ -1,6 +1,12 @@
 { config, pkgs, lib, ... }: {
-  system.keyboard = {
-    enableKeyMapping = true;
-    remapCapsLockToControl = true;
+  system = {
+    keyboard = {
+      enableKeyMapping = true;
+      remapCapsLockToControl = true;
+    };
+    defaults.NSGlobalDomain = {
+      InitialKeyRepeat = 10;
+      KeyRepeat = 2;
+    };
   };
 }
